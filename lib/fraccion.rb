2071a1164d53a4c3c0c76c37284f27098eb78e26
alu0100819182 <alu0100819182@ull.edu.es>
2015-10-28 18:53:52 +0000

    Creada las operaciones aritmeticas con fracciones

---
+++ lib/fraccion.rb
@@ -0,0 +1,40 @@
+class Fracion
+	attr reader :numerador, :denominador
+	
+	def maycomdiv(a,b)
+		b!=0? a: gcd(b, a%b)
+	end
+	
+	def mincommult(a,b)
+		z = a / maycomdiv(a,b) * b
+    end
+	
+	def initialize(num,denom)
+		raise TypeError, "Denominator may not be zero" if denom.eql? 0
+		aux = maycomdiv(num,denom)
+		@numerador = num/d; #obtengo el minimo
+		@denominador = denom/d;
+    end
+   
+   def + (other)
+   		mcmden = mincommult(@denominador, other.denominador)
+   		aux1 = (mcmden / @denominador) * @numerador
+   		aux2 = (mcmden / other.denominador) * other.numerador
+   		Fracion.new(aux1 + aux2,mcmden)
+   end
+   
+   def - (other)
+		mcmden = mincommult(@denominador, other.denominador)
+   		aux1 = (mcmden / @denominador) * @numerador
+   		aux2 = (mcmden / other.denominador) * other.numerador
+   		Fracion.new(aux1 - aux2,mcmden)
+   end
+   
+   def * (other)
+   		Fracion.new(@numerador*other.numerador, @denominador*other.denominador)
+   end
+   
+   def / (other)
+   		Fracion.new(@numerador*other.denominador, @denominador*other.numerador)
+   
+   
